c66fb50c41efd604188f8b1223ff25d5cb87bdc8
RELNO <noyman@Ariels-MacBook-Pro.local>
2018-06-14 16:32:00 -0400

    cleared p lib

--- Test_Table/test.js
+++ Test_Table/test.js
@@ -78,20 +78,17 @@ async function viz(jsonData) {
         if (jsonData.grid[i] === null) {
             cells[i].style.backgroundColor = 'rgba(0,0,0,0.25)';
             cells[i].innerHTML = 'no type';
-            cells[i].id = 'p'
-            p(cells[i]);
-
 
         } else if (typeKey >= 0 && typeKey < 10) {
-            cells[i].style.backgroundColor = 'rgba(242, 216, 46,0.5)';
+            cells[i].style.backgroundColor = 'rgba(242, 216, 46,0.35)';
             cells[i].innerHTML = 'live ' + typeKey;
 
         } else if (typeKey > 11 && typeKey < 20) {
-            cells[i].style.backgroundColor = 'rgba(114, 10, 76,0.5)';
+            cells[i].style.backgroundColor = 'rgba(200, 100, 0,0.35)';
             cells[i].innerHTML = 'work ' + typeKey;
 
         } else if (typeKey > 20 && typeKey <= 21) {
-            cells[i].style.backgroundColor = 'rgba(100, 160, 77,0.5)';
+            cells[i].style.backgroundColor = 'rgba(100, 160, 77,0.35)';
             cells[i].innerHTML = 'park';
         }
     }
@@ -99,121 +96,6 @@ async function viz(jsonData) {
 //get key of brick's type in cityIO info
 function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
-}
-
-
-//
-function p(div) {
-    particlesJS(div.id, {
-        "particles": {
-            "number": {
-                "value": 200,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#ffffff"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 1,
-                    "color": "#000000"
-                },
-                "polygon": {
-                    "nb_sides": 5
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 3,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 150,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 6,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": false,
-                    "mode": "grab"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 140,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": true
-    });
 }
 
 
