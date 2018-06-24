3c586a84df5939fd8f3775a02384e6b6bc61a689
RELNO <noyman@Ariels-MacBook-Pro.local>
2018-06-24 12:28:28 -0400

    added stats

--- js/main.js
+++ js/main.js
@@ -746,3 +746,18 @@ function UI() {
   //cityIO link
   cityioFolder.add(parm, "fe").name("View on cityIO Dashboard");
 }
+
+//stats
+javascript: (function() {
+  var script = document.createElement("script");
+  script.onload = function() {
+    var stats = new Stats();
+    document.body.appendChild(stats.dom);
+    requestAnimationFrame(function loop() {
+      stats.update();
+      requestAnimationFrame(loop);
+    });
+  };
+  script.src = "//rawgit.com/mrdoob/stats.js/master/build/stats.min.js";
+  document.head.appendChild(script);
+})();
