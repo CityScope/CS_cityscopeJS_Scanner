97fc67cce4d29b1a4cebd9b469e6f5366a13ddf5
RELNO <noyman@dhcp-18-111-118-7.dyn.MIT.EDU>
2018-08-12 23:17:49 -0400

    cleanup

--- js/UI/UItools.js
+++ js/UI/UItools.js
@@ -126,3 +126,28 @@ export function stats() {
   script.src = "//rawgit.com/mrdoob/stats.js/master/build/stats.min.js";
   document.head.appendChild(script);
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export function makeInfoDiv() {
+  //make info div
+  let infoDiv = document.createElement("div");
+  infoDiv.id = "infoDiv";
+  infoDiv.className = "info";
+  document.body.appendChild(infoDiv);
+  return infoDiv;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//make info div [on screen console] or add text to it
+export function UpdateinfoDiv(text) {
+  var infoDiv = Storage.infoDiv;
+  // clear div if too much text
+  if (infoDiv.scrollHeight > 2000) {
+    infoDiv.innerHTML = null;
+  } else {
+    infoDiv.innerHTML += text.toString() + "<p></p>";
+    infoDiv.scrollTop = infoDiv.scrollHeight;
+  }
+}
